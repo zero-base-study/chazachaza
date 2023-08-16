5124212640cb47489265be1de8ac40d335c590ff
southoftheriver <anwjsrlrhwkd@naver.com>
2023-08-16 14:11:47 +0900

    spring boot settting

---
+++ src/main/java/zerobase/demo/ChazachazaApplication.java
@@ -0,0 +1,13 @@
+package zerobase.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChazachazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChazachazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zerobase/demo/ChazachazaApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChazachazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
